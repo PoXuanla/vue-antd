d6e7f503715588ad77b804c9f811965f757c24ce
zzki <wdftmmas234@gmail.com>
2023-11-07 23:37:41 +0530

    test glob

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,8 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 
-
+const modules = import.meta.glob("/src/**/*.vue");
+console.log(
+  modules["/src/views/TableView.vue"]().then((mod) => console.log(mod))
+);
 </script>
